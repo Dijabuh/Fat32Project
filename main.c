bf1d6a8993d8edbf0f2bfbcd4e529cda4ff9f07e
Daniel Brown <djb16b@my.fsu.edu>
2020-04-18 12:18:26 -0400

    Added cd function

--- main.c
+++ main.c
@@ -223,7 +223,7 @@ void size_cmd(struct BPB* bpb, int file, pathparts* cmd, unsigned int start_clus
 	if(cmd->numParts == 2) {
 		//run size on dir name given if it exists
 		//loop through dir entry for given start cluster
-		//if name of one of them matches given name, set its lo/hi first cluster to clus
+		//if name of one of them matches given name, print its size
 		int quit = 0;
 		while(clus > 0) {
 			//get data location from cluster number
@@ -238,7 +238,16 @@ void size_cmd(struct BPB* bpb, int file, pathparts* cmd, unsigned int start_clus
 				struct DIRENTRY de;
 				get_dir_entry(&de, file, location);
 	
-				if(strcmp(de.dir_name, cmd->parts[1]) == 0) {
+				
+				if(de.dir_name[0] == 0x00) {
+					printf("File %s does not exists\n", cmd->parts[1]);
+					return;
+				}
+
+				if(de.dir_attr == 0x0F) {
+					//long name entry, do nothing
+				}
+				else if(strcmp(de.dir_name, cmd->parts[1]) == 0) {
 					if(de.dir_attr != 0x20){
 						printf("%s is not a file\n", cmd->parts[1]);
 						return;
@@ -263,6 +272,57 @@ void size_cmd(struct BPB* bpb, int file, pathparts* cmd, unsigned int start_clus
 	}
 	else {
 		printf("Wrong number of arguements for size command\n");
+	}
+}
+
+unsigned int cd_cmd(struct BPB* bpb, int file, pathparts* cmd, unsigned int star_cluster) {
+	unsigned int clus = star_cluster;
+
+	if(cmd->numParts == 2) {
+		//run cd on dir name given if it exists
+		//loop through dir entry for given start cluster
+		//if name of one of them matches given name, return its start cluster
+		while(clus > 0) {
+			//get data location from cluster number
+			unsigned int location = get_data_location(clus, bpb);
+			location -= 32;
+
+			//loop through all dir entries here
+			for(int i = 0; i < bpb->bytes_per_sec/32; i++) {
+				location += 32;
+	
+				//get dir entry
+				struct DIRENTRY de;
+				get_dir_entry(&de, file, location);
+	
+				if(de.dir_name[0] == 0x00) {
+					printf("Directory %s does not exists\n", cmd->parts[1]);
+					return 0;
+				}
+
+				if(de.dir_attr == 0x0F) {
+					//long name entry, do nothing
+				}
+				else if(strcmp(de.dir_name, cmd->parts[1]) == 0) {
+					if(de.dir_attr != 0x10){
+						printf("%s is not a directory\n", cmd->parts[1]);
+						return 0;
+					}
+					if((de.hi_fst_clus << 16) + de.lo_fst_clus == 0) {
+						return 2;
+					}
+					return ((de.hi_fst_clus << 16) + de.lo_fst_clus);
+				}
+			}
+			//get next cluster
+			clus = get_next_cluster(clus, bpb, file);
+		}
+		printf("Directory %s does not exists\n", cmd->parts[1]);
+		return 0;
+	}
+	else {
+		printf("Wrong number of arguements for cd command\n");
+		return 0;
 	}
 }
 
@@ -302,6 +362,13 @@ int main(int argc, char** argv) {
 		else if(strcmp(cmd.parts[0], "size") == 0) {
 			size_cmd(&bpb, file, &cmd, start_cluster);
 		}
+		else if(strcmp(cmd.parts[0], "cd") == 0) {
+			int ret = cd_cmd(&bpb, file, &cmd, start_cluster);
+			if(ret > 0) {
+				start_cluster = ret;
+			}
+		}
+
 	}
 
 	return 0;
